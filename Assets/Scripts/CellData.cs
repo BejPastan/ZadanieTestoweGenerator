b184c0cd63a17357fa5f45ded678c8b681862c04
BejPastan <s.synaszko@gmail.com>
2024-04-25 11:40:01 +0200

    Generating rivers Part I
    Now only generate rivers, in progress generating lakes on the end of rivers

--- Assets/Scripts/CellData.cs
+++ Assets/Scripts/CellData.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,13 +8,16 @@ public struct CellData
     public GroundType ground { get; private set; }
     public Transform element { get; private set; }
     public Vector2 slope { get; private set; }
-
+    public List<Elements> elements { get; private set; }
+    private List<Transform> elementsTransform;
     public CellData(float height, GroundType ground, Vector2 slope, Transform element)
     {
         this.height = height;
         this.ground = ground;
         this.element = element;
         this.slope = slope;
+        elements = new List<Elements>();
+        elementsTransform = new List<Transform>();
 
         if(element.GetComponent<SpriteRenderer>() == null )
         {
@@ -21,6 +25,15 @@ public struct CellData
         }
 
         element.GetComponent<SpriteRenderer>().sprite = WorldGenerator.instance.elementSprite.GetSprite(ground);
+    }
+
+    public void AddElement(Elements element, Transform elementTrans)
+    {
+        elements.Add(element);
+        elementsTransform.Add(elementTrans);
+        elementTrans.transform.SetParent(this.element);
+        elementTrans.transform.localPosition = Vector3.zero;
+        elementTrans.GetComponentInChildren<SpriteRenderer>().sprite = WorldGenerator.instance.elementSprite.GetSprite(element);
     }
 }
 
@@ -31,6 +44,11 @@ public enum GroundType
     water
 }
 
+public enum Elements
+{
+    river,
+    forest
+}
 
 [CreateAssetMenu(fileName = "ElementSprite", menuName = "ElementSprite")]
 public class ElementSprite : ScriptableObject
@@ -40,6 +58,11 @@ public class ElementSprite : ScriptableObject
     [SerializeField]
     Sprite[] sprite;
 
+    [SerializeField]
+    Elements[] elements;
+    [SerializeField]
+    Sprite[] elementSprite;
+
     public Sprite GetSprite(GroundType groundType)
     {
         for (int i = 0; i < this.groundType.Length; i++)
@@ -47,6 +70,18 @@ public class ElementSprite : ScriptableObject
             if (this.groundType[i] == groundType)
             {
                 return sprite[i];
+            }
+        }
+        return null;
+    }
+
+    public Sprite GetSprite(Elements element)
+    {
+        for (int i = 0; i < this.elements.Length; i++)
+        {
+            if (this.elements[i] == element)
+            {
+                return elementSprite[i];
             }
         }
         return null;
--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -1,8 +1,6 @@
-using System.Collections.Generic;
-using TreeEditor;
-using Unity.Mathematics;
-using Unity.VisualScripting;
+using System;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class WorldGenerator : MonoBehaviour
 {
@@ -21,6 +19,8 @@ public class WorldGenerator : MonoBehaviour
 
     [SerializeField]
     GameObject cellPref;
+    [SerializeField]
+    GameObject elementPref;
 
     [SerializeField]
     MeshRenderer meshRenderer;
@@ -28,10 +28,10 @@ public class WorldGenerator : MonoBehaviour
     private void Start()
     {
         Grid.instance.SetMapSize(settings.width, settings.length);
-        GenerateBase();
+        GenerateWorld();
     }
 
-    private void GenerateBase()
+    private void GenerateWorld()
     {
         Vector2[,] slopeGradient;
         float[,] heightMap;
@@ -39,6 +39,11 @@ public class WorldGenerator : MonoBehaviour
 
         Grid.instance.Cells = new CellData[settings.width, settings.length];
         VisualiseHeightMap(ref heightMap, ref slopeGradient);
+        for(int i = 0; i < settings.riverCount; i++)
+        {
+            Vector2Int[] river = WaterGenerator.GenerateWater(ref heightMap);
+            VisualiseRivers(ref river);
+        }
     }
 
     public void VisualiseHeightMap(ref float[,] heightMap, ref Vector2[,] slopeMap)
@@ -68,6 +73,7 @@ public class WorldGenerator : MonoBehaviour
             for (int z = 0; z < settings.length; z++)
             {
                 Transform cell = Instantiate(cellPref, new Vector3(x, z, 0), Quaternion.identity).transform;
+                cell.name = $"Cell_{x}_{z}";
 
                 switch (heightMap[x, z])
                 {
@@ -96,6 +102,59 @@ public class WorldGenerator : MonoBehaviour
                 }
 
             }
+        }
+    }
+
+    public void VisualiseRivers(ref Vector2Int[] riverMap)
+    {
+        for (int i = 0; i < riverMap.Length; i++)
+        {
+            Vector2Int river = riverMap[i];
+            //check if river is not on the water
+            if (Grid.instance.Cells[river.x, river.y].ground == GroundType.water)
+            {
+                break;
+            }
+            //create river object
+            
+            //calc river position
+            Vector2 cross1, cross2;
+            if(i == 0)
+            {
+                cross1 = riverMap[i];
+            }
+            else
+            {
+                cross1 = riverMap[i] + riverMap[i-1];
+                cross1 /= 2;
+            }
+
+            if(i == riverMap.Length-1)
+            {
+                cross2 = riverMap[i];
+            }
+            else
+            {
+                cross2 = riverMap[i] + riverMap[i + 1];
+                cross2 /= 2;
+            }
+            Debug.Log($"cross1: {cross1}, cross2: {cross2}");
+            //calc angle beetwen points
+            float angle = Mathf.Atan2(cross2.y - cross1.y, cross2.x - cross1.x) * Mathf.Rad2Deg;
+            //calc distance beetwen points
+            float distance = Vector2.Distance(cross1, cross2);
+            Vector2 pos = (cross1 + cross2) / 2;
+            Debug.Log($"angle: {angle}, distance: {distance}, pos: {pos}");
+
+
+            Transform riverObj = Instantiate(elementPref, new Vector3(river.x, river.y, 0), Quaternion.identity).transform;
+            riverObj.GetChild(0).localScale = new Vector3(distance, 1, 1);
+            //set rotation from angle
+            riverObj.GetChild(0).localRotation = Quaternion.Euler(0, 0, angle);
+            riverObj.GetChild(0).position = new Vector3(pos.x, pos.y, 0);
+            //add river to cell
+            Grid.instance.Cells[river.x, river.y].AddElement(Elements.river, riverObj);
+            Debug.Log("river: " + river);
         }
     }
 }
@@ -210,12 +269,123 @@ public static class HeightMapGenerator
 
 public static class WaterGenerator
 {
-    public static Vector2Int GenerateWater(ref float[,] heightMap, ref Vector2[,] gradientMap)
+    public static Vector2Int[] GenerateWater(ref float[,] heightMap)
     {
-        
+        //get random point
+        Vector2Int startPoint = new Vector2Int(UnityEngine.Random.Range(0, heightMap.GetLength(0)), UnityEngine.Random.Range(0, heightMap.GetLength(1)));
+        return GenerateRiver(ref heightMap, startPoint, out Vector2Int[]lakes);
+    }
+
+    public static Vector2Int[] GenerateRiver(ref float[,] heightMap, Vector2Int startPoint, out Vector2Int[] lakes)
+    {
+        lakes = new Vector2Int[0];
+        Vector2Int[] river = new Vector2Int[1];
+        river[0] = startPoint;
+        for (int i = 0; i < 1000; i++)
+        {
+            Vector2Int currentPoint = river[river.Length - 1];
+            //get lowest neighbour
+            Vector2Int[] neighbour = GetLowestNeighbour(currentPoint, ref heightMap);
+
+            //check if lowest neighbour is water or is out of bounds or is the same as current point
+            Vector2Int lowestNeighbour = new Vector2Int();
+            for (int j = 0; j < neighbour.Length; j++)
+            {
+                if
+                (
+                    Grid.instance.Cells[neighbour[j].x, neighbour[j].y].ground == GroundType.water ||
+                    neighbour[j].x < 0 || neighbour[j].x >= heightMap.GetLength(0) ||
+                    neighbour[j].y < 0 || neighbour[j].y >= heightMap.GetLength(1)
+                )
+                {
+                    continue;
+                }
+                else
+                {
+                    if (heightMap[neighbour[j].x, neighbour[j].y] == heightMap[currentPoint.x, currentPoint.y])
+                    {
+                        break;
+                        //end generating river
+                        //generate lake
+                    }
+                    lowestNeighbour = neighbour[j];
+                    break;
+                }
+            }
+
+            //if lowest neighbour is higher than current point break
+            if (heightMap[lowestNeighbour.x, lowestNeighbour.y] >= heightMap[currentPoint.x, currentPoint.y])
+            {
+                Debug.Log("break at: " + i);
+                break;
+            }
+            Array.Resize(ref river, river.Length + 1);
+            river[river.Length - 1] = lowestNeighbour;
+        }
+
+        return river;
+    }
+
+    private static Vector2Int[] GetLowestNeighbour(Vector2Int point, ref float[,] heightMap)
+    {
+        Vector2Int[] neighbours = new Vector2Int[0];
+
+        //get cells from around the current cell
+        for (int i = -1; i < 2; i++)
+        {
+            for (int j = -1; j < 2; j++)
+            {
+                if ((i == 0 && j == 0) || i+point.x < 0 || i+point.x >= heightMap.GetLength(0) || j+point.y < 0 || j+point.y >= heightMap.GetLength(1))
+                {
+                    continue;
+                }
+                Vector2Int neighbour = new Vector2Int(point.x + i, point.y + j);
+                Array.Resize(ref neighbours, neighbours.Length + 1);
+                neighbours[neighbours.Length - 1] = neighbour;
+            }
+        }
+
+        //sort neighbours by height
+        HeightMapService.SortByHeight(ref neighbours);
+
+
+        return neighbours;
+
+    }
+
+    private static Vector2Int[] GenerateLake(Vector2Int startPoint, int minSize)
+    {
+        int currentSize = 0;
+        do
+        {
+            
+        }while(currentSize < minSize);
+
+
+        return null;
     }
 }
 
+
+public static class HeightMapService
+{
+    public static void SortByHeight(ref Vector2Int[] cells)
+    {
+        float[,] heightMap = Grid.instance.GetHeightMap();
+        for (int i = 0; i < cells.Length; i++)
+        {
+            for (int j = 0; j < cells.Length; j++)
+            {
+                if (heightMap[cells[i].x, cells[i].y] < heightMap[cells[j].x, cells[j].y])
+                {
+                    Vector2Int temp = cells[i];
+                    cells[i] = cells[j];
+                    cells[j] = temp;
+                }
+            }
+        }
+    }
+}
 
 [CreateAssetMenu(fileName = "WorldSettings", menuName = "WorldSettings")]
 public class WorldSettings : ScriptableObject
@@ -226,4 +396,6 @@ public class WorldSettings : ScriptableObject
     public float maxElevation, minElevation;
 
     public float mainScale;
+
+    public int riverCount;
 }
